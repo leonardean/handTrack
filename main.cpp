f99e28117921690b4428513516fb30c42edf4455
Zhanwen_Xu <leonardoipod@gmail.com>
2014-01-07 15:24:39 +0800

    duplicate removal

--- main.cpp
+++ main.cpp
@@ -2,8 +2,6 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
 using namespace std;
 using namespace cv;
 //This function returns the square of the euclidean distance between 2 points.
@@ -32,91 +30,10 @@ pair<Point,double> circleFromPoints(Point p1, Point p2, Point p3)
 	return make_pair(Point(centerx,centery),radius);
 }
 
-//When called , it simulates a click at the curent mouse cursor location
-void mouseClick()
-{
-	int button=Button1;
-	Display *display = XOpenDisplay(NULL);
-
-	XEvent event;
-
-	if(display == NULL)
-	{
-		cout<<"Error connecting to display"<<endl;
-		exit(EXIT_FAILURE);
-	}
-	memset(&event, 0x00, sizeof(event));
-
-	event.type = ButtonPress;
-	event.xbutton.button = button;
-	event.xbutton.same_screen = True;
-	XQueryPointer(display, RootWindow(display, DefaultScreen(display)), &event.xbutton.root, &event.xbutton.window, &event.xbutton.x_root, &event.xbutton.y_root, &event.xbutton.x, &event.xbutton.y, &event.xbutton.state);
-	event.xbutton.subwindow = event.xbutton.window;
-	while(event.xbutton.subwindow)
-	{
-		event.xbutton.window = event.xbutton.subwindow;
-		XQueryPointer(display, event.xbutton.window, &event.xbutton.root, &event.xbutton.subwindow, &event.xbutton.x_root, &event.xbutton.y_root, &event.xbutton.x, &event.xbutton.y, &event.xbutton.state);
-	}
-
-	if(XSendEvent(display, PointerWindow, True, 0xfff, &event) == 0) cout<<"ERROR SENDING CLICK"<<endl;
-	
-	XFlush(display);
-
-	XCloseDisplay(display);
-}
-
-//When called, it simulates a mouse release event at the current cursor location
-void mouseRelease()
-{
-	int button=Button1;
-	Display *display = XOpenDisplay(NULL);
-
-	XEvent event;
-
-	if(display == NULL)
-	{
-		cout<<"Error connecting to display"<<endl;
-		exit(EXIT_FAILURE);
-	}
-	memset(&event, 0x00, sizeof(event));
-
-	event.xbutton.button = button;
-	event.xbutton.same_screen = True;
-	XQueryPointer(display, RootWindow(display, DefaultScreen(display)), &event.xbutton.root, &event.xbutton.window, &event.xbutton.x_root, &event.xbutton.y_root, &event.xbutton.x, &event.xbutton.y, &event.xbutton.state);
-	event.xbutton.subwindow = event.xbutton.window;
-	while(event.xbutton.subwindow)
-	{
-		event.xbutton.window = event.xbutton.subwindow;
-		XQueryPointer(display, event.xbutton.window, &event.xbutton.root, &event.xbutton.subwindow, &event.xbutton.x_root, &event.xbutton.y_root, &event.xbutton.x, &event.xbutton.y, &event.xbutton.state);
-	}
-
-
-	event.type = ButtonRelease;
-	event.xbutton.state = 0x100;
-
-	if(XSendEvent(display, PointerWindow, True, 0xfff, &event) == 0) cout<<"ERROR RELEASING"<<endl;
-
-	XFlush(display);
-
-	XCloseDisplay(display);
-}
-
-
-//This function moves the mouse cursor to (x,y)
-void mouseTo(int x,int y)
-{
-	 Display *display = XOpenDisplay(0);
-	  Window root = DefaultRootWindow(display);
-	  XWarpPointer(display, None, root, 0, 0, 0, 0, x, y);
-	  XFlush(display);
-	  XCloseDisplay(display);
-}
-
-
 //The main function :D
 int main(int argc, char *argv[])
 {
-	string filename = "/Users/new-worker/Desktop/hand2.mov";
+	string filename = "/Users/new-worker/OpenCVHandGuesture/hand.mov";
 	Mat frame;
 	Mat back;
 	Mat fore;
